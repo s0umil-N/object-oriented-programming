94eaf2d4959366e3a5a10cc9daf863e5040b8205
s0umil-N <soumil.16616@stu.upes.ac.in>
2026-01-29 12:33:30 +0530

    static 3

---
+++ static_3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class static_3 { 
+    public static void main (String[]args){
+    int n;
+    Scanner s = new Scanner (System.in);
+    System.out.print("enter a number");
+    n = s.nextInt();  
+    System.out.println(+n);
+    s.close();
+    }
+}
+
